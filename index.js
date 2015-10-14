45d7ce2313e4e84f31ddefcd9e8674224cd81af8
Josh Johnston <josh@x-team.com>
2015-10-14 11:39:41 +1100

    added a shortcut to getCss()

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ var loader = require('./loader')
 
 var _root
 
-module.exports = function (dir) {
-  return loader(dir, _root)
-}
-
-module.exports.init = function (dir) {
+function init (dir) {
   _root = new loader.Loader(dir)
   return _root
 }
 
+module.exports = function (dir) {
+  dir = dir || process.cwd()
+  if (!_root) { init(dir) }
+
+  return loader(dir, _root)
+}
+
+module.exports.init = init
+module.exports.getCss = function () { return _root.getCss() }
 module.exports.build = require('./build')
